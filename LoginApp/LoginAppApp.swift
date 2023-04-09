b5bd4426c80ca7ab2e12e5b759dbebcc4c837057
kelsey <kelsey.collins23@d155.org>
2023-04-09 10:52:25 -0500

    Initial Commit

---
+++ LoginApp/LoginAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginAppApp.swift
+//  LoginApp
+//
+//  Created by Timothy Collins on 4/9/23.
+//
+
+import SwiftUI
+
+@main
+struct LoginAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
