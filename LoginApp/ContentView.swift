aa03d2dc11232697e07e56ec1a33a2ef41b1f7b8
ok <kelsey.collins23@d155.org>
2023-04-18 07:56:07 -0500

    login screen done fix navigation segue

--- LoginApp/ContentView.swift
+++ LoginApp/ContentView.swift
@@ -8,16 +8,60 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var username = ""
+    @State private var password = ""
+    @State private var wrongpass = 0
+    @State private var wronguser = 0
+    @State private var screen = false
+
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView{
+            VStack {
+                Text("Login")
+                    .font(.largeTitle)
+                TextField("Username:", text: $username)
+                    .frame(width: 300,height: 50)
+                    .background(Color.black.opacity(0.05))
+                    .border(.red,width: CGFloat(wronguser))
+                SecureField("Password:", text: $password)
+                    .frame(width: 300,height: 50)
+                    .background(Color.black.opacity(0.05))
+                    .border(.red,width: CGFloat(wrongpass))
+                Button("Enter") {
+                    check(username: username, password: password)
+                    if(wronguser == 0 && wrongpass == 0 ){
+                        
+                    }
+                }
+                .frame(width: 300,height: 50)
+                .background(Color.blue.opacity(0.05))
+                
+            }
         }
-        .padding()
+        .navigationBarHidden(true)
+            .padding()
+            
+        }
+    func check(username: String, password: String){
+        if username.lowercased() == "panini"{
+            wronguser = 0
+            if password.lowercased() == "forever"{
+             wrongpass = 0
+             }
+             else{
+             wrongpass = 2
+             }
+             }
+             else{
+             wronguser = 2
+             }
+             
+        }
     }
-}
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
---
+++ LoginApp/LoggedInView.swift
@@ -0,0 +1,20 @@
+//
+//  LoggedInView.swift
+//  LoginApp
+//
+//  Created by KELSEY COLLINS on 4/10/23.
+//
+
+import SwiftUI
+
+struct LoggedInView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoggedInView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoggedInView()
+    }
+}
